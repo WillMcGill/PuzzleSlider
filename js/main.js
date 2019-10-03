a0beb04820611622d77e30526ba7b882f683a4f7
Will McGIll <willmcgill@Makiyas-iPhone.localdomain>
2019-10-03 10:36:42 -0400

    built tile objects

--- js/main.js
+++ js/main.js
@@ -6,8 +6,6 @@ var count = 0;
 
 //create board
 
-
-
 for (let i = 1; i <= 4; i++){
     var newRow = document.createElement("div");
     newRow.className = "row border";
@@ -25,3 +23,49 @@ for (let i = 1; i <= 4; i++){
 
         
 }
+//Define the tile constructor
+
+
+function Tile(idx, currPos, y, x, z) {
+    this.x = x;
+    this.y = y;
+    this.z = z;
+    this.idx = idx;
+    this.currPos = currPos;
+    
+  }
+// Create Tiles
+  function MovingTiles(idx, currPos, x, y, z, color) {
+    Tile.call(this, idx, currPos, x, y, z);
+    this.color = color;
+  }
+
+  var tileArr =  [];
+  let coordX = 0;
+  let coordY = 0; 
+// Create Moving Tile Objects
+  for (let i = 1; i <= 15; i++){
+
+      console.log(i);
+      let movingTiles =  new MovingTiles(
+          i, 
+          i,
+          coordX,
+          coordY,
+          0,
+          "rgb(255,222,173)"
+      );
+
+      if (coordX < 3){
+          coordX++;
+      }
+      else {coordY++;
+            coordX = 0};
+
+      tileArr.push(movingTiles);
+  }
+    
+
+
+MovingTiles.prototype = Object.create(Tile.prototype);
+MovingTiles.prototype.constructor = MovingTiles;
