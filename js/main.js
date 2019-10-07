592dbbcbf9208ab62461db3c1b0c2229552aed5d
Will McGIll <willmcgill@Makiyas-iPhone.localdomain>
2019-10-07 16:49:59 -0400

    added randomize

--- js/main.js
+++ js/main.js
@@ -2,9 +2,9 @@ var app = document.getElementById("app");
 app.className = "container-fluid p-5 border";
 
 var count = 0;
-var tileArr =  [];
+var tileArr = [];
 let coordX = 0;
-let coordY = 0; 
+let coordY = 0;
 var getDiv = 0;
 //var blankTile = "";
 
@@ -15,59 +15,61 @@ function Tile(idx, currPos, x, y, tileType) {
     this.x = x;
     this.y = y;
     this.tileType = tileType;
-    
-    
-  }
+
+
+}
 // Create Tiles
-  function MovingTiles(idx, currPos, x, y, tileType, color) {
+function MovingTiles(idx, currPos, x, y, tileType, color) {
     Tile.call(this, idx, currPos, x, y, tileType);
     this.color = color;
-  }
+}
 
-  
+
 // Create Moving Tile Objects
-  for (let i = 1; i <= 16; i++){
+for (let i = 1; i <= 16; i++) {
 
-      //console.log(i);
-      let movingTiles =  new MovingTiles(
-          i, 
-          i,
-          coordX,
-          coordY,
-          0,
-          "rgb(255,222,173)"
-      );
+    //console.log(i);
+    let movingTiles = new MovingTiles(
+        i,
+        i,
+        coordX,
+        coordY,
+        0,
+        "rgb(255,255,255)"
+    );
 
-      if (coordX < 3){
-          coordX++;
-      }
-      else  {coordY++;
-            coordX = 0};
+    if (coordX < 3) {
+        coordX++;
+    }
+    else {
+        coordY++;
+        coordX = 0
+    };
 
-        if (movingTiles.idx == 16){   // blank tile
-            movingTiles.tileType = 1;
-            movingTiles.color = "rgb(255,255,255)"
-        }
-        //console.log(movingTiles.idx);
-        
-      tileArr.push(movingTiles);
+    if (movingTiles.idx == 16) {   // blank tile
+        movingTiles.tileType = 1;
+        movingTiles.color = "rgb(255,255,255)"
+    }
+    //console.log(movingTiles.idx);
 
-    
-  }
+    tileArr.push(movingTiles);
 
-  //console.log(tileArr); 
+
+}
+
+//console.log(tileArr); 
 
 MovingTiles.prototype = Object.create(Tile.prototype);
 MovingTiles.prototype.constructor = MovingTiles;
 
 //create board
-function renderNewBoard(){
-for (let i = 1; i <= 4; i++){
-    var newRow = document.createElement("div");
-    newRow.className = "row border";
-    app.appendChild(newRow);
+function renderNewBoard() {
+    for (let i = 1; i <= 4; i++) {
+        var newRow = document.createElement("div");
+        newRow.className = "row border";
+        app.appendChild(newRow);
 
-        for (let j = 1; j <= 4; j++){
+        for (let j = 1; j <= 4; j++) {
             var newCol = document.createElement("div");
             newCol.className = "col-3 border mx-auto";
             newCol.id = Number(count);
@@ -80,48 +82,57 @@ for (let i = 1; i <= 4; i++){
             newCol.appendChild(tile);
             count++;
         }
-}}
+    }
+    var newRow = document.createElement("div");
+    newRow.className = "row";
+    var randomButt = document.createElement("button");
+    randomButt.className = "btn btn-info mx-auto";
+    randomButt.addEventListener("click", randomizeBoard);
+    app.appendChild(newRow);
+    newRow.appendChild(randomButt);
+    randomButt.innerHTML = "Randomize";
+}
 
-function clickHandler(){
-    
+function clickHandler() {
+
 
     getDiv = Number(this.id);
     var n = 0;
 
-    for (n = 0; n <= 15; n++){
+    for (n = 0; n <= 15; n++) {
         //console.log({n});
-        if (tileArr[n].tileType == 1){
+        if (tileArr[n].tileType == 1) {
             //var blank = n;
-            console.log("blank tile - ",n);
+            console.log("blank tile - ", n);
             break;
-            };
+        };
     }
-    console.log({getDiv});
+    console.log({ getDiv });
     blankTestSwitch(n);
     renderBoardOnClick(n);
-    
-    
+
+
 }
 
 renderNewBoard();
 
-function blankTestSwitch(n){
-    
+function blankTestSwitch(n) {
+
     let tempX = "";
     let tempY = "";
     let tempPos = "";
-    
+
     //switch coordinates
-    
-    tempX = tileArr[n].x   
+
+    tempX = tileArr[n].x
     tileArr[n].x = tileArr[getDiv].x;
     tileArr[getDiv].x = tempX;
-   
-    tempY = tileArr[n].y;    
+
+    tempY = tileArr[n].y;
     tileArr[n].y = tileArr[getDiv].y;
     tileArr[getDiv].y = tempY;
 
-    tempPos = tileArr[n].currPos;    
+    tempPos = tileArr[n].currPos;
     tileArr[n].currPos = tileArr[getDiv].currPos;
     tileArr[getDiv].currPos = tempPos;
 
@@ -129,60 +140,49 @@ function blankTestSwitch(n){
 
     tileArr[n].tileType = 0;
     tileArr[getDiv].tileType = 1;
-    
+
 
 }
 
-function renderBoardOnClick(n){
+function renderBoardOnClick(n) {
 
-    
+
     let clickTile = document.getElementById(getDiv).innerHTML;
     let blankTile = document.getElementById(n).innerHTML;
 
-    console.log({clickTile, blankTile,});
+    console.log({ clickTile, blankTile, });
 
     document.getElementById(getDiv).innerHTML = blankTile;
     document.getElementById(n).innerHTML = clickTile;
 
-
-    
 }
+
+function randomizeBoard(){
+
+    for (let i = 1; i <= 100; i++){
     
-    // for (let i = 0; i <= 15; i++){
         
-    //     console.log('test');
-    //     let a = tileArr[i].currPos;
-    //     let b = tileArr[i].idx;
-    //     console.log('current:', a, 'text:', b);
-    //     document.getElementById(a - 1).innerHTML = b;
-        
-    // }
     
-    //console.log({tileArr});
-    //console.log("before render - ",n, getDiv);
-    
-    // let clickedTile = document.getElementById(getDiv).innerHTML;
-    // let blankTile = document.getElementById(5).innerHTML;
-    // console.log({n, getDiv});
-    // console.log(document.getElementById(n).innerHTML);
-    // // console.log({clickedTile,blankTile});
-    // // console.log(`elementbyId(${blank} = ${clickedTile}`)
-    // // console.log(`elementbyId(${getDiv} = ${blankTile}`)
-    // // console.log({blank,getDiv});
-    // //console.log(tileArr[getDiv].idx)
-    // // document.getElementById(n) = clickedTile;
-    // // document.getElementById(getDiv) = blankTile;
-    // //getDiv = tileArr[n].currPos;
-    // console.log({clickedTile, blankTile});
-    // document.getElementById(clickedTile - 1).innerHTML = blankTile;
-    // document.getElementById(blankTile - 1).innerHTML = clickedTile;
-    // //console.log({clickedTile, blankTile});
-    
+    getDiv = Math.floor(Math.random() * 16);
+    var n = 0;
+
+    for (n = 0; n <= 15; n++) {
+        //console.log({n});
+        if (tileArr[n].tileType == 1) {
+            //var blank = n;
+            console.log("blank tile - ", n);
+            break;
+        };
+    }
+    console.log({ getDiv });
+    blankTestSwitch(n);
+    renderBoardOnClick(n);}
+
+}
 
 
 
-    
 
 
-    
- 
+
+
